8d2f77f95ae9b26e04ebb6810c0207ce3ac9cebd
Ali <150237117+avrojo@users.noreply.github.com>
2025-10-11 00:26:27 +0200

    radial gradient!

--- utils/gradient.js
+++ utils/gradient.js
@@ -1,9 +1,22 @@
-$(document).mousemove(function (event) {
-	windowWidth = $(window).width();
-	windowHeight = $(window).height();
+// $(document).mousemove(function (event) {
+// 	windowWidth = $(window).width();
+// 	windowHeight = $(window).height();
 
-	mouseXpercentage = Math.round(event.pageX / windowWidth * 100);
-	mouseYpercentage = Math.round(event.pageY / windowHeight * 100);
+// 	mouseXpercentage = Math.round(event.pageX / windowWidth * 100);
+// 	mouseYpercentage = Math.round(event.pageY / windowHeight * 100);
 
-	$('.nombre').css('background', 'radial-gradient(circle at ' + mouseXpercentage + '% ' + mouseYpercentage + '%,  #a4c281ff, #e1ecd5)');
-});
+// 	$('.nombre').css('background', 'radial-gradient(circle at ' + mouseXpercentage + '% ' + mouseYpercentage + '%,  #a4c281ff, #e1ecd5)');
+// });
+
+const move = document.getElementById("move");
+
+document.body.onpointermove = event => {
+	const { clientX, clientY } = event;
+
+	move.animate({
+		left: `${clientX}px`,
+		top: `${clientY}px`
+
+	}, { duration: 1000, fill: "forwards" })
+
+}
