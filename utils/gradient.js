e34b1751f5bfe67548763d928126db31966f1c4b
Ali <150237117+avrojo@users.noreply.github.com>
2025-10-21 22:03:55 +0200

    formatted

--- utils/gradient.js
+++ utils/gradient.js
@@ -1,13 +1,3 @@
-// $(document).mousemove(function (event) {
-// 	windowWidth = $(window).width();
-// 	windowHeight = $(window).height();
-
-// 	mouseXpercentage = Math.round(event.pageX / windowWidth * 100);
-// 	mouseYpercentage = Math.round(event.pageY / windowHeight * 100);
-
-// 	$('.nombre').css('background', 'radial-gradient(circle at ' + mouseXpercentage + '% ' + mouseYpercentage + '%,  #a4c281ff, #e1ecd5)');
-// });
-
 const move = document.getElementById("move");
 
 document.body.onpointermove = event => {
